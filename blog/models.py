d130681f4ae7a6163cf2a7c4eeb33205b74b8f18
Blaz <blaz.kosica@gmail.com>
2022-10-31 16:54:08 +0000

    In admin.py add (POST model), install Summernote (add it to requirements.txt, in settings.py add INSTALLED_APPS), in urls.py add (‘include’  and new path under urlpatterns), in admin.py add (class PostAdmin, delete method on line 12, add decorator above class) fix typos in models.py

--- blog/models.py
+++ blog/models.py
@@ -34,19 +34,19 @@ class Post(models.Model):
         return self.likes.count()
 
 
-# Likes Model
+# Comment Model
 
 
-class Coment(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
     name = models.CharField(max_length=80)
     email = models.EmailField()
     body = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
-    aproved = models.BooleanField(default=False)
+    approved = models.BooleanField(default=False)
 
     class Meta:
-        ordering = ['created_on']
+        ordering = ["created_on"]
 
     def __str__(self):
         return f"Comment {self.body} by {self.name}"
